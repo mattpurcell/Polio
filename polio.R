2a68383154ab163cacf065a10c3709e392b863b0
mattpurcell <mattpurcell4@gmail.com>
2018-03-20 09:57:58 -0400

    Add files via upload

---
+++ polio.R
@@ -0,0 +1,12 @@
+setwd("~/desktop/polio")
+
+require('tidyr')
+h = read.csv('HDI_original.csv')
+h_long = gather(h, year, HDI, X2015:X1990)
+h_long$year = as.numeric(substr(h_long$year, 2, 5))
+
+d_long = read.csv('incidence-long.csv')
+
+c = read.csv('coverage.csv')
+c_long = gather(c, year, coverage, X2016:X1980)
+c_long$year = as.numeric(substr(c_long$year, 2, 5))
