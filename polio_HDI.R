e42783967426a54ebaa593b2a1fbc7020bca8fce
mattpurcell <mattpurcell4@gmail.com>
2018-04-01 23:02:41 -0400

    Update polio_HDI.R
    added negative binomial

--- polio_HDI.R
+++ polio_HDI.R
@@ -1,4 +1,5 @@
-#setwd("~/desktop/polio")
+setwd("~/desktop/polio")
+
 
 require('tidyr')
 h = read.csv('HDI_original.csv')
@@ -13,6 +14,8 @@ c = read.csv('coverage.csv')
 c_long = gather(c, year, coverage, X2016:X1980)
 c_long$year = as.numeric(substr(c_long$year, 2, 5))
 
+
+
 merged_data = merge(c_long, h_long[,c('Country', 'year', 'HDI')])
 merged_data = merge(merged_data, d_long[,c('Country', 'year', 'incidence')])
 merged_data$coverage = merged_data$coverage/100
@@ -23,6 +26,7 @@ HDI = merged_data$HDI
 coverage = merged_data$coverage
 
 logreg = glm(incidence!=0 ~ HDI + coverage, family = binomial(link = 'logit'))
-
+nbreg = glm(incidence!=0 ~ HDI + coverage,family = negative.binomial(theta = 1))
 summary(logreg)
+summary(nbreg)
 detach(merged_data)
